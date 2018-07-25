6b7894fd98f5b2c45820f030bb02e65eddee9a16
Andre Odendaal <aodendaal@gmail.com>
2018-07-25 10:44:42 +0200

    Create new mop tool and include it in the scene

---
+++ UnityProject/Assets/Scripts/Tools/Mop.cs
@@ -0,0 +1,58 @@
+﻿using Items;
+using PlayGroup;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Mop : PickUpTrigger
+{
+	[SyncVar] public NetworkInstanceId ControlledByPlayer;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (ControlledByPlayer == NetworkInstanceId.Invalid)
+		{
+			return;
+		}
+
+		//don't start it too early:
+		if (!PlayerManager.LocalPlayer)
+		{
+			return;
+		}
+
+		//Only update if it is inhand of localplayer
+		if (PlayerManager.LocalPlayer != ClientScene.FindLocalObject(ControlledByPlayer))
+		{
+			return;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			ChatRelay.Instance.AddToChatLogClient("You mop the floor", ChatChannel.Local);
+		}
+	}
+
+	#region Weapon Pooling
+
+	//This is only called on the serverside
+	public void OnAddToPool(NetworkInstanceId ownerId)
+	{
+		ControlledByPlayer = ownerId;
+	}
+
+	public void OnRemoveFromPool()
+	{
+		ControlledByPlayer = NetworkInstanceId.Invalid;
+	}
+
+	#endregion
+}
